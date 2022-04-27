cbb26bf5013034f92321ad513854aa17bb2a0ffe
Manon Verjus <93917350+manonvjs@users.noreply.github.com>
2022-04-27 13:58:12 +0200

    Update script.Rmd
    Ajout de packages

--- script/setup.R
+++ script/setup.R
@@ -1,6 +1,55 @@
 #############################################
 ## The following loads the needed packages ##
 #############################################
+#############################################
+## The following loads the needed packages ##
+#############################################
+
+# load the required packages
+packages <- c(
+  "knitr",
+  "here", # for the project's organization
+  "tidyverse", "kableExtra","bookdown", "rmarkdown", "DT",
+  "usethis", # git
+  "ggplot2", "dplyr","readr","plotly","viridis", "summarytools", "caret", "lattice", "hrbrthemes","extrafontdb", "Rttf2pt1","extrafont","gdtools","GGally",
+  "patchwork", "dygraphs", "gganimate", "lattice","maps", "cluster", "factoextra", "gganimate", "xtable", "shiny", "datasets")
+#for plotting
+
+
+purrr::walk(packages, library, character.only = TRUE)
+
+######################################################
+## The following sets a few option for nice reports ##
+######################################################
+
+# general options
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut"),
+  width = 69,
+  tibble.width = 69,
+  cli.unicode = FALSE
+)
+
+# ggplot options
+theme_set(theme_light())
+
+# knitr options
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  dpi = 300,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,
+  fig.show = "hold",
+  message = FALSE,
+ echo = FALSE
+ )
+
 
 # load the required packages
 packages <- c(
