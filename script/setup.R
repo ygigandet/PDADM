782138afed57be5f0863f7a6d83770bc5ffc58d4
Manon Verjus <93917350+manonvjs@users.noreply.github.com>
2022-04-27 13:59:29 +0200

    Update script.Rmd - 27.04
    Ajout de packages

--- script/setup.R
+++ script/setup.R
@@ -1,9 +1,6 @@
 #############################################
 ## The following loads the needed packages ##
 #############################################
-#############################################
-## The following loads the needed packages ##
-#############################################
 
 # load the required packages
 packages <- c(
@@ -49,48 +46,3 @@ knitr::opts_chunk$set(
   message = FALSE,
  echo = FALSE
  )
-
-
-# load the required packages
-packages <- c(
-  "knitr",
-  "here", # for the project's organization
-  "tidyverse", "kableExtra",
-  "usethis", # git
-  "plotly", "patchwork" #for plotting
-)
-
-purrr::walk(packages, library, character.only = TRUE)
-
-######################################################
-## The following sets a few option for nice reports ##
-######################################################
-
-# general options
-options(
-  digits = 3,
-  str = strOptions(strict.width = "cut"),
-  width = 69,
-  tibble.width = 69,
-  cli.unicode = FALSE
-)
-
-# ggplot options
-theme_set(theme_light())
-
-# knitr options
-knitr::opts_chunk$set(
-  comment = "#>",
-  collapse = TRUE,
-  cache = TRUE,
-  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
-  dpi = 300,
-  out.width = "70%",
-  fig.align = "center",
-  fig.width = 6,
-  fig.asp = 0.618,
-  fig.show = "hold",
-  message = FALSE,
- echo = FALSE
- )
-
