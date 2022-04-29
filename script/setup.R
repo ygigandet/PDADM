8fdb97e91d5652b434b3cef3a4f1862329020e21
manonvjs <manon.verjus@unil.ch>
2022-04-29 15:27:15 +0200

    - added a feature selection to select important variables (6 selected by the model - added 2 packages in the script

--- script/setup.R
+++ script/setup.R
@@ -9,7 +9,7 @@ packages <- c(
   "tidyverse", "kableExtra","bookdown", "rmarkdown", "DT",
   "usethis", # git
   "ggplot2", "readr", "plotly", "viridis", "caret", "lattice", "hrbrthemes", "extrafontdb", "Rttf2pt1", "extrafont", "gdtools", "GGally",
-  "patchwork", "dygraphs", "gganimate", "lattice", "maps", "cluster", "factoextra", "gganimate", "xtable", "shiny", "datasets"
+  "patchwork", "dygraphs", "gganimate", "lattice", "maps", "cluster", "factoextra", "gganimate", "xtable", "shiny", "datasets", "caret", "mlbench"
 )
 
 purrr::walk(packages, library, character.only = TRUE)
