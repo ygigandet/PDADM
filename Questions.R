751f35f5f450e34419aca1942b7eae088b2961eb
ygigandet <yooby.gigandet@gmail.com>
2022-05-05 18:13:49 +0200

    Questions to ask to the teacher

---
+++ Questions.R
@@ -0,0 +1,25 @@
+# Questions to the client:
+#   
+# CHK_ACCT (Checking accounts)
+#   The first category means that the checking accounts are in negative? Right so there is no limit at zero?
+# 
+# HISTORY (Credit history)
+#   Is it really the case that the credit applications with a bad history are more graded as positive?
+#     
+# SAV_ACCT (Saving accounts)
+#  Why if the client do not have a saving accounts at the bank, then the application is better rated? 
+#    What is the different then between a small saving accounts and none?
+#    
+# GUARANTOR
+#  Category 3? Is it a mistake?
+#    
+# EDUCATION
+#  -1, again a mistake?
+#    
+# PRESENT_RESID
+#  The categories should be between 0 and 3 but are between 1 and 4? What is the interpretation?
+#    
+# ANSWERS:
+#    
+# AGE:
+#    One outlier to recode - 75 years old instead of 125 years old
